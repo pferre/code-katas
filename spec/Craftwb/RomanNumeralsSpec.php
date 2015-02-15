2f09a83a326e3fc21522eb19f452c053b34199d0
Pierre Ferré <pierre@pierreferre.com>
2015-02-15 13:43:39 +0000

    roman numerals complete with tests

--- spec/Craftwb/RomanNumeralsSpec.php
+++ spec/Craftwb/RomanNumeralsSpec.php
@@ -17,11 +17,6 @@ class RomanNumeralsSpec extends ObjectBehavior
 		$this->romanNumeralFor(2)->shouldReturn('II');
 	}
 
-	function it_returns_III_for_3()
-	{
-		$this->romanNumeralFor(3)->shouldReturn('III');
-	}
-
 	function it_returns_IV_for_4()
 	{
 		$this->romanNumeralFor(4)->shouldReturn('IV');
@@ -31,4 +26,47 @@ class RomanNumeralsSpec extends ObjectBehavior
 	{
 		$this->romanNumeralFor(5)->shouldReturn('V');
 	}
+
+	function it_returns_VI_for_6()
+	{
+		$this->romanNumeralFor(6)->shouldReturn('VI');
+	}
+
+	function it_returns_IX_for_9()
+	{
+		$this->romanNumeralFor(9)->shouldReturn('IX');
+	}
+
+	function it_returns_X_for_10()
+	{
+		$this->romanNumeralFor(10)->shouldReturn('X');
+	}
+
+	function it_returns_XI_for_11()
+	{
+		$this->romanNumeralFor(11)->shouldReturn('XI');
+	}
+
+	function it_returns_XLIX_for_49()
+	{
+		$this->romanNumeralFor(49)->shouldReturn('XLIX');
+	}
+
+	function it_returns_L_for_50()
+	{
+		$this->romanNumeralFor(50)->shouldReturn('L');
+	}
+
+	function it_returns_XCIX_for_99()
+	{
+		$this->romanNumeralFor(99)->shouldReturn('XCIX');
+	}
+
+	function it_returns_C_for_100()
+	{
+		$this->romanNumeralFor(100)->shouldReturn('C');
+	}
+
+
+
 }
--- src/Craftwb/RomanNumerals.php
+++ src/Craftwb/RomanNumerals.php
@@ -4,25 +4,46 @@ namespace Craftwb;
 
 class RomanNumerals
 {
+	protected static $lookup = [
+		100	=>	'C',
+		99	=>	'XCIX',
+		50	=> 	'L',
+		49	=>	'XLIX',
+		10	=>	'X',
+		9	=>	'IX',
+		5	=>	'V',
+		4	=>	'IV',
+		1   =>	'I'
+	];
 
+	/**
+	 * 	@param number
+	 *	@return string	
+	 */
     public function romanNumeralFor($number)
     {
-    	if ($number == 2) {
-    		return 'II';
+    	$this->checkForValidNumber($number);
+
+    	$roman = '';
+
+    	foreach (static::$lookup as $limit => $value) {
+    		while ($number >= $limit) {
+    			$roman .= $value;
+    			$number -= $limit;
+    		}
     	}
 
-    	if ($number == 3) {
-    		return 'III';
-    	}
+    	return $roman;
+    }
 
-    	if ($number == 4) {
-    		return 'IV';
+    /**
+     * 	@param number
+     *	@return number|InvalidArgumentException
+     */
+    private function checkForValidNumber($number)
+    {
+    	if ($number <= 0) {
+    		return new InvalidArgumentException();
     	}
-
-    	if ($number == 5) {
-    		return 'V';
-    	}
-
-        return 'I';
     }
 }
